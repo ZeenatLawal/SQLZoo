97a42d09963ed6f7ac6b30e4b130e42064c94371
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-18 14:43:10 +0100

    add some answers to SELECT from WORLD Tutorial

--- answers.sql
+++ answers.sql
@@ -1,4 +1,4 @@
---SELECT basics
+-- SELECT basics
 
 -- 1. Introducing the world table of countries
 SELECT population FROM world
@@ -11,3 +11,42 @@ SELECT name, population FROM world
 -- 3. Just the right size
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000;
+
+
+-- SELECT from WORLD Tutorial
+
+-- 1. Introduction
+SELECT name, continent, population FROM world;
+
+-- 2. Large Countries
+SELECT name
+  FROM world
+ WHERE population > 200000000;
+
+-- 3. Per capita GDP
+SELECT name, GDP/population
+  FROM world
+ WHERE population > 200000000;
+
+-- 4. South America In millions
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America';
+
+-- 5. France, Germany, Italy
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+-- 6. United
+SELECT name
+FROM world
+WHERE name LIKE '%United%';
+
+-- 7. Introduction
+-- 8. Introduction
+-- 9. Introduction
+-- 10. Introduction
+-- 11. Introduction
+-- 12. Introduction
+-- 13. Introduction
