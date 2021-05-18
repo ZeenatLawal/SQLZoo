c5fb78fadc104794940e05b3e1986f647b1f9dac
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-18 14:46:39 +0100

    add answers to SELECT from WORLD Tutorial

--- answers.sql
+++ answers.sql
@@ -43,10 +43,38 @@ SELECT name
 FROM world
 WHERE name LIKE '%United%';
 
--- 7. Introduction
--- 8. Introduction
--- 9. Introduction
--- 10. Introduction
--- 11. Introduction
--- 12. Introduction
--- 13. Introduction
+-- 7. Two ways to be big
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000;
+
+-- 8. One or the other (but not both)
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 XOR population > 250000000;
+
+-- 9. Rounding
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+FROM world
+WHERE continent = 'South America';
+
+-- 10. Trillion dollar economies
+SELECT name, ROUND(GDP/population/1000)*1000
+FROM world
+WHERE GDP > 1000000000000;
+
+-- 11. Name and capital have the same length
+SELECT name, capital
+FROM world
+ WHERE LENGTH(name) = LENGTH(capital);
+
+-- 12. Matching name and capital
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital;
+
+-- 13. All the vowels
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+  AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
