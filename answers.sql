d7e1f889cbf4ed3c3a93992f687e0c4fe5fcd9d1
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-18 16:32:41 +0100

    add answers to the SELECT from Nobel Tutorial

--- answers.sql
+++ answers.sql
@@ -78,3 +78,76 @@ SELECT name
    FROM world
 WHERE name LIKE '%a%'
   AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+
+
+-- SELECT from Nobel Tutorial
+
+-- 1. Winners from 1950
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
+
+-- 2. 1962 Literature
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+-- 3. Albert Einstein
+SELECT yr, subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein';
+
+-- 4. Recent Peace Prizes
+SELECT winner
+  FROM nobel
+ WHERE subject = 'Peace' AND yr >= 2000;
+
+-- 5. Literature in the 1980's
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
+
+-- 6. Only Presidents
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter', 'Barack Obama');
+
+-- 7. John
+SELECT winner FROM nobel
+ WHERE winner LIKE 'John%';
+
+-- 8. Chemistry and Physics from different years
+SELECT yr, subject, winner
+FROM nobel
+ WHERE (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 1984);
+
+-- 9. Exclude Chemists and Medics
+SELECT yr, subject, winner
+FROM nobel
+ WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+-- 10. Early Medicine, Late Literature
+SELECT yr, subject, winner
+FROM nobel
+ WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
+
+-- 11. Umlaut
+SELECT * FROM nobel
+ WHERE winner = 'PETER GRÜNBERG';
+
+-- 12. Apostrophe
+SELECT * FROM nobel
+ WHERE winner = 'EUGENE O''NEILL';
+
+-- 13. Knights of the realm
+SELECT winner, yr, subject FROM nobel
+ WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner ASC;
+
+-- 14. Chemistry and Physics last
+SELECT winner, subject
+  FROM nobel
+ WHERE yr = 1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject, winner;
