23b50be5085cf2fc928e79e0929b5ed885bf6399
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-19 10:01:12 +0100

    add answers for the SELECT within SELECT Tutorial

--- answers.sql
+++ answers.sql
@@ -151,3 +151,67 @@ SELECT winner, subject
   FROM nobel
  WHERE yr = 1984
  ORDER BY subject IN ('Physics','Chemistry'), subject, winner;
+
+-- SELECT within SELECT Tutorial
+
+-- 1. Bigger than Russia
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia');
+
+-- 2. Richer than UK
+SELECT name FROM world 
+WHERE continent = 'Europe' AND gdp/population > 
+  (SELECT gdp/population FROM world 
+    WHERE name = 'United Kingdom');
+
+-- 3. Neighbours of Argentina and Australia
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent
+  FROM world
+  WHERE name IN ('Argentina', 'Australia'))
+  ORDER BY name;
+
+-- 4. Between Canada and Poland
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world 
+  WHERE name = 'Canada') AND population < (SELECT population FROM world 
+    WHERE name = 'Poland');
+
+-- 5. Percentages of Germany
+SELECT name, CONCAT(ROUND(100*population/
+  (SELECT population FROM world WHERE name = 'Germany')), '%') AS percentage
+FROM world
+WHERE continent = 'Europe';
+
+-- 6. Bigger than every country in Europe
+SELECT name FROM world 
+  WHERE gdp > (SELECT MAX(gdp) FROM world 
+    WHERE continent = 'Europe');
+
+-- 7. Largest in each continent
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0);
+
+-- 8. First country of each continent (alphabetically)
+SELECT continent, name FROM world x
+  WHERE name <= ALL
+    (SELECT name FROM world y
+        WHERE y.continent=x.continent);
+
+-- 9. Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+SELECT name, continent, population
+FROM world x
+WHERE 25000000 >= ALL(SELECT population FROM world y WHERE x.continent = y.continent);
+
+-- 10. Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+SELECT name, continent FROM world x
+  WHERE population >= ALL
+    (SELECT population * 3 FROM world y
+        WHERE y.continent=x.continent AND x.name <> y.name);
