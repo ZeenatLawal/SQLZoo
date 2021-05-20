9779c837193aa2ef27363e10051bd21f0c8e20f7
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-20 10:14:24 +0100

    add answers for the Self join tutorial

--- answers.sql
+++ answers.sql
@@ -466,3 +466,68 @@ SELECT teacher.name,
 CASE WHEN teacher.dept IN (1,2) THEN 'Sci'
 WHEN teacher.dept IN (3) THEN 'Art'
 ELSE 'None' END AS dept FROM teacher;
+
+-- Self join
+
+-- 1. How many stops are in the database.
+SELECT COUNT(*) FROM stops;
+
+-- 2. Find the id value for the stop 'Craiglockhart'
+SELECT id FROM stops WHERE name = 'Craiglockhart';
+
+-- 3. Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT id, name FROM stops
+JOIN route ON stops.id = stop
+WHERE company = 'LRT' AND num = '4';
+
+-- 4. Routes and stops
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) > 1;
+
+-- 5.
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = 149;
+
+-- 6.
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road';
+
+-- 7.
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=115 AND b.stop = 137;
+
+-- 8.
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross';
+
+-- 9.
+SELECT DISTINCT stopb.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart';
+
+-- 10.
+SELECT a.num, a.company,  stops.name,  d.num, d.company
+FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+JOIN stops ON b.stop = stops.id
+JOIN route c ON c.stop = stops.id
+JOIN route d ON c.company = d.company AND c.num = d.num
+WHERE a.stop = (SELECT id FROM stops WHERE name = 'Craiglockhart')
+AND d.stop = (SELECT id FROM stops WHERE name = 'Lochend')
+ORDER BY a.num, stops.name, d.num;
